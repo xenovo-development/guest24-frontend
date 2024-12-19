a3bb01f2e27de907bdbc7d343249ca361a0a67ca
damlahocaoglu <damlahocaogluu@gmail.com>
2024-12-19 12:42:10 +0300

    update testimonial

--- components/Home/Testimonial.vue
+++ components/Home/Testimonial.vue
@@ -1,15 +1,13 @@
 <template>
-  <!-- Widget testimonial -->
   <section class="pd-main bg-1">
     <div class="tf-container">
       <div class="row">
         <div class="col-lg-12">
           <div class="center m0-auto w-text-heading mb-40">
-            <span
-              class="sub-title-heading text-main fs-28-46 font-yes wow fadeInUp animated"
-              >Explore the world</span
-            >
-            <h2 class="title-heading wow fadeInUp animated">
+            <span class="sub-title-heading text-main fs-28-46 font-yes">
+              Explore the world
+            </span>
+            <h2 class="title-heading">
               Amazing Featured Tour
               <span class="text-gray font-yes">Package</span> the world
             </h2>
@@ -18,110 +16,87 @@
       </div>
       <div class="row">
         <div class="col-lg-12 relative">
-          <div class="swiper testimonialSwiper overflow-hidden pb-60">
-            <div class="swiper-wrapper">
-              <div class="swiper-slide">
-                <div class="widget-testimonial-style2 relative">
-                  <div class="flex-two mb-25">
-                    <div class="flex-three">
-                      <div class="image">
-                        <img
-                          src="/img/testimonial/avata.jpg"
-                          alt="Image Testimonial"
-                        />
-                      </div>
-                      <div class="profile">
-                        <h5 class="name">Cameron Williamson</h5>
-                        <span class="job">Ceo & Founder</span>
-                      </div>
+          <ClientOnly>
+            <swiper-container
+              ref="containerRef"
+              :slides-per-view="2"
+              :space-between="30"
+              :autoplay="{ delay: 3000, disableOnInteraction: false }"
+              loop="true"
+              class="testimonialSwiper"
+              :breakpoints="{
+                640: {
+                  slidesPerView: 1,
+                  spaceBetween: 20,
+                },
+                1024: {
+                  slidesPerView: 2,
+                  spaceBetween: 30,
+                }
+              }"
+            >
+              <swiper-slide
+                v-for="(testimonial, index) in testimonials"
+                :key="index"
+                class="widget-testimonial-style2 relative"
+              >
+                <div class="flex-two mb-25">
+                  <div class="flex-three">
+                    <div class="image">
+                      <img :src="testimonial.image" alt="Image Testimonial" />
                     </div>
-                    <div class="icon-tes flex-five">
-                      <i class="icon-Layer_x0020_1"></i>
-                    </div>
-                  </div>
-                  <p class="des">
-                    The most advanced revenue than this. Iwill refer everyone I
-                    like Level more and more each day because it makes my
-                    easier. It really saves me time and effort. Level is exactly
-                    business has been lacking.
-                  </p>
-                  <img
-                    src="/img/testimonial/mask.png"
-                    class="mask-tes"
-                    alt="Mask"
-                  />
-                </div>
-              </div>
-              <div class="swiper-slide">
-                <div class="widget-testimonial-style2 relative">
-                  <div class="flex-two mb-25">
-                    <div class="flex-three">
-                      <div class="image">
-                        <img
-                          src="/img/testimonial/avata.jpg"
-                          alt="Image Testimonial"
-                        />
-                      </div>
-                      <div class="profile">
-                        <h5 class="name">Cameron Williamson</h5>
-                        <span class="job">Ceo & Founder</span>
-                      </div>
-                    </div>
-                    <div class="icon-tes flex-five">
-                      <i class="icon-Layer_x0020_1"></i>
+                    <div class="profile">
+                      <h5 class="name">{{ testimonial.name }}</h5>
+                      <span class="job">{{ testimonial.job }}</span>
                     </div>
                   </div>
-                  <p class="des">
-                    The most advanced revenue than this. Iwill refer everyone I
-                    like Level more and more each day because it makes my
-                    easier. It really saves me time and effort. Level is exactly
-                    business has been lacking.
-                  </p>
-                  <img
-                    src="/img/testimonial/mask.png"
-                    class="mask-tes"
-                    alt="Mask"
-                  />
-                </div>
-              </div>
-              <div class="swiper-slide">
-                <div class="widget-testimonial-style2 relative">
-                  <div class="flex-two mb-25">
-                    <div class="flex-three">
-                      <div class="image">
-                        <img
-                          src="/img/testimonial/avata.jpg"
-                          alt="Image Testimonial"
-                        />
-                      </div>
-                      <div class="profile">
-                        <h5 class="name">Cameron Williamson</h5>
-                        <span class="job">Ceo & Founder</span>
-                      </div>
-                    </div>
-                    <div class="icon-tes flex-five">
-                      <i class="icon-Layer_x0020_1"></i>
-                    </div>
+                  <div class="icon-tes flex-five">
+                    <i class="icon-Layer_x0020_1"></i>
                   </div>
-                  <p class="des">
-                    The most advanced revenue than this. Iwill refer everyone I
-                    like Level more and more each day because it makes my
-                    easier. It really saves me time and effort. Level is exactly
-                    business has been lacking.
-                  </p>
-                  <img
-                    src="/img/testimonial/mask.png"
-                    class="mask-tes"
-                    alt="Mask"
-                  />
                 </div>
-              </div>
-            </div>
-            <div class="swiper-pagination"></div>
-          </div>
+                <p class="des">{{ testimonial.description }}</p>
+              </swiper-slide>
+            </swiper-container>
+          </ClientOnly>
         </div>
       </div>
     </div>
   </section>
-  <!-- Widget testimonial -->
 </template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { Swiper as SwiperSlide } from "swiper/vue";
+
+const containerRef = ref(null);
+const testimonials = ref([
+  {
+    name: "Lorem Ipsum",
+    job: "CEO & Founder",
+    image: "/img/testimonial/avata.jpg",
+    description:
+      "The most advanced revenue than this. I will refer everyone I like Level more and more each day because it makes my easier. It really saves me time and effort. Level is exactly business has been lacking.",
+  },
+  {
+    name: "Lorem Ipsum Sit",
+    job: "CEO & Founder",
+    image: "/img/testimonial/avata.jpg",
+    description:
+      "The most advanced revenue than this. I will refer everyone I like Level more and more each day because it makes my easier. It really saves me time and effort. Level is exactly business has been lacking.",
+  },
+  {
+    name: "Lorem Ipsum Sit Amet",
+    job: "CEO & Founder",
+    image: "/img/testimonial/avata.jpg",
+    description:
+      "The most advanced revenue than this. I will refer everyone I like Level more and more each day because it makes my easier. It really saves me time and effort. Level is exactly business has been lacking.",
+  },
+]);
+
+let swiperInstance;
+
+onMounted(() => {
+  swiperInstance = containerRef.value?.swiper;
+  console.log(swiperInstance);
+});
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/i18n"],
+  modules: ["@nuxtjs/i18n","nuxt-swiper"],
   app: {
     head: {
       title: "Guest24 - To find the unforgettable",
